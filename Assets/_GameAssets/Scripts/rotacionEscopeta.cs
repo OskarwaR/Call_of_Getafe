17c65e5d5a7a4ede01c879010a9ea9c85df0e847
OskarwaR <oescudero82@gmail.com>
2021-05-19 17:04:08 +0200

    armas y huesos

---
+++ Assets/_GameAssets/Scripts/rotacionEscopeta.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotacionEscopeta : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float x;
+    private float y;
+    private float z;
+    private float cx;
+    private float cy;
+    private float cz;
+    private object player;
+    void Start()
+    {
+        x = transform.localPosition.x;
+        y = transform.localPosition.y;
+        z = transform.localPosition.z;
+        player= GameObject.FindGameObjectsWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cx = Camera.main.transform.localPosition.x;
+        cy = Camera.main.transform.localPosition.x;
+        cz = GameObject.FindGameObjectWithTag("Player").transform.position.z;
+        transform.localPosition = new Vector3((cx/2)+0.7f, (cy/2)-0.5f, z);
+
+    }
+}
