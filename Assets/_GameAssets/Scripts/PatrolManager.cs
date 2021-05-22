e220e1b9c1384a06034c8b17332fedc87b0a08a2
OskarwaR <oescudero82@gmail.com>
2021-05-22 16:06:32 +0200

    cafe

--- Assets/_GameAssets/Scripts/PatrolManager.cs
+++ Assets/_GameAssets/Scripts/PatrolManager.cs
@@ -6,18 +6,28 @@ using UnityEngine.AI;
 public class PatrolManager : MonoBehaviour
 {
     public Transform[] patrolPoints;
+    private List<Transform> patrolList = new List<Transform>();
     private NavMeshAgent nma;
+    private GameObject lista;
     public int currentPoint = 0;
     private void Awake()
     {
+        lista = GameObject.Find("RutaPatruya");
         nma = GetComponent<NavMeshAgent>();
+
+        foreach (Transform child in lista.transform)
+        {
+            // do whatever you want with child transform object here
+            print("Foreach loop: " + child + " " + patrolList.Count);
+            patrolList.Add(child);
+        }
     }
     // Start is called before the first frame update
     void Start()
     {
         nma.SetDestination(patrolPoints[currentPoint].transform.position);
         //Ñapa
-        GetComponentInChildren<Animator>().SetBool("Walk", true);
+        GetComponentInChildren<Animator>().SetBool("Run", true);
     }
 
     // Update is called once per frame
@@ -27,7 +37,9 @@ public class PatrolManager : MonoBehaviour
         {
             currentPoint++;
             if (currentPoint == patrolPoints.Length) currentPoint = 0;
-            nma.SetDestination(patrolPoints[currentPoint].transform.position);
+            //nma.SetDestination(patrolPoints[currentPoint].transform.position);
+            //nma.SetDestination(patrolPoints[Random.Range(0,patrolPoints.Length)].transform.position);
+            nma.SetDestination(patrolList[Random.Range(0, patrolList.Count)].transform.position);
         }
     }
 }
